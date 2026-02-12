6e3b3dd5e14b0cf5c02b92344ba228681f5301ca
Shollz <augustineogunlade1@gmail.com>
2026-02-12 21:29:22 +0000

    best code

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "dev/terraform.state"
+    key            = "dev/terraform.tfstate"
     bucket         = "auggie-bootcamp-bucket"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
--- uat/backend.tf
+++ uat/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "prod/terraform.state"
+    key            = "prod/prod-auggie-cluster"
     bucket         = "auggie-bootcamp-bucket"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
